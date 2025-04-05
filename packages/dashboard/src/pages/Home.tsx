5283b20d55361ca2cea6830c6484630ff490de68
CarolineMorton12 <cm2781@bath.ac.uk>
2025-04-06 00:56:27 +0100

    homepage done without data

--- packages/dashboard/src/pages/Home.tsx
+++ packages/dashboard/src/pages/Home.tsx
@@ -1,26 +1,40 @@
 import { useState } from "react";
 import laptop from "../images/generalLaptop.svg";
 import logo from "../images/wattLogo.svg";
+import happyDuck from "../images/happyDuck.svg";
 
 export default function HomePage() {
   const [leftBoxOpen, setLeftBoxOpen] = useState(false);
   const [rightBoxOpen, setRightBoxOpen] = useState(false);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-start bg-gray-50">
+    <div className="min-h-screen flex flex-col items-center justify-start bg-gradient-to-r from-blue-500 to-indigo-600">
       {/* Logo */}
-      <div className="w-full flex justify-center my-2">
-        <img src={logo} alt="Site Logo" className="w-[500px] object-contain" />
+      <div className="w-full flex justify-center">
+        <img
+          src={logo}
+          alt="Site Logo"
+          className="w-[500px] object-scale-down"
+        />
       </div>
 
-      {/* Laptop section */}
+      {/* Laptop + Duck */}
       <div className="relative">
+        {/* Laptop */}
         <img src={laptop} alt="General Laptop" className="w-[500px]" />
+
+        {/* Happy Duck (positioned over the laptop screen) */}
+        <img
+          src={happyDuck}
+          alt="Happy Duck"
+          className="absolute w-[500px]"
+          style={{ top: "10%", left: "75%", transform: "translateX(-50%)" }}
+        />
 
         {/* Buttons on keyboard */}
         <div
           className="z-20 absolute flex flex-col items-center space-y-2"
-          style={{ top: "61%", left: "47%", transform: "translateX(-50%)" }}
+          style={{ top: "63%", left: "50%", transform: "translateX(-50%)" }}
         >
           <button
             onClick={() => setLeftBoxOpen(!leftBoxOpen)}
@@ -39,7 +53,7 @@ export default function HomePage() {
         {/* Side Boxes */}
         {leftBoxOpen && (
           <div
-            className="absolute top-1/2 -translate-y-1/2 w-60 h-60 bg-blue-500 text-white border shadow-xl p-4 rounded-xl z-10"
+            className="absolute top-1/2 -translate-y-1/2 w-60 h-60 bg-red-500 text-white border shadow-xl p-4 rounded-xl z-10"
             style={{ left: "-260px" }}
           >
             <p className="text-base font-semibold">Total Energy and Carbon Use</p>
